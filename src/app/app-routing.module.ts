bdf062600431d2c55099a8bd12ec7810c37b428b
René Kok <rkok98@gmail.com>
2020-09-28 13:09:41 +0200

    Update add dish component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {AddDishComponent} from "./views/add-dish/add-dish.component";
+import { AddDishComponent } from "./views/add-dish/add-dish.component";
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
-  { path: 'add-dish', component: AddDishComponent },
-  { path: 'welcome', loadChildren: () => import('./views/welcome/welcome.module').then(m => m.WelcomeModule) },
-
+  {path: '', pathMatch: 'full', redirectTo: '/welcome'},
+  {path: 'add-dish', component: AddDishComponent},
+  {path: 'welcome', loadChildren: () => import('./views/welcome/welcome.module').then(m => m.WelcomeModule)},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { IconsProviderModule } from './icons-provider.module';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NZ_I18N } from 'ng-zorro-antd/i18n';
@@ -14,7 +15,7 @@ import { nl_NL } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import nl from '@angular/common/locales/nl';
 import { AddDishComponent } from './views/add-dish/add-dish.component';
-import {NzFormModule, NzInputModule, NzButtonModule, NzInputNumberModule} from "ng-zorro-antd";
+import { NzFormModule, NzInputModule, NzButtonModule, NzInputNumberModule } from "ng-zorro-antd";
 
 registerLocaleData(nl);
 
@@ -25,6 +26,7 @@ registerLocaleData(nl);
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     IconsProviderModule,
     NzLayoutModule,
@@ -36,9 +38,10 @@ registerLocaleData(nl);
     NzInputModule,
     ReactiveFormsModule,
     NzButtonModule,
-    NzInputNumberModule
+    NzInputNumberModule,
   ],
-  providers: [{ provide: NZ_I18N, useValue: nl_NL }],
+  providers: [{provide: NZ_I18N, useValue: nl_NL}],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
