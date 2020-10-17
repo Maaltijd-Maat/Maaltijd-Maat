77412a76b35daa521b29f0c384fab176609f36d8
René Kok <rkok98@gmail.com>
2020-10-17 17:41:02 +0200

    Renamed authenticateService to AuthenticateService.

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import {authenticateService} from '@services/authenticate/authenticate.service';
-import {Credentials} from '@models:/credentials';
+import { AuthenticateService } from '@services/authenticate/authenticate.service';
+import { Credentials } from '@models:/credentials';
 
 @Component({
   selector: 'app-login-component',
@@ -12,9 +12,9 @@ export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
   alertMessage?: string;
 
-  constructor(private fb: FormBuilder, private authenticateService: authenticateService) {}
+  constructor(private fb: FormBuilder, private authenticateService: AuthenticateService) {}
 
-  ngOnInit() : void{
+  ngOnInit(): void {
     this.loginForm = this.fb.group({
       email: [null, [Validators.required]],
       password: [null, [Validators.required]],
@@ -28,13 +28,13 @@ export class LoginComponent implements OnInit {
       this.loginForm.controls[i].updateValueAndValidity();
     }
 
-    if (this.loginForm.valid){
+    if (this.loginForm.valid) {
       const credentials = new Credentials(this.loginForm.controls['email'].value, this.loginForm.controls['password'].value);
       this.authenticateService.authenticateUser(credentials).then(
-        (res) => {
+        () => {
           this.alertMessage = 'success';
         },
-        (err) => { this.alertMessage = 'error';}
+        () => { this.alertMessage = 'error';}
       );
     }
   }
