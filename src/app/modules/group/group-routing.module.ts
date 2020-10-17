282f5aef890af9be60d0ae3205318bdbd37b3061
René Kok <rkok98@gmail.com>
2020-10-17 16:33:14 +0200

    Resolver

--- src/app/modules/group/group-routing.module.ts
+++ src/app/modules/group/group-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GroupsComponent } from './pages/groups/groups.component';
+import { GroupsResolver } from './pages/groups/groups.resolver';
 
 const routes: Routes = [
-  { path: '', component: GroupsComponent }
+  { path: '', component: GroupsComponent, resolve: { groups: GroupsResolver } }
 ];
 
 @NgModule({
