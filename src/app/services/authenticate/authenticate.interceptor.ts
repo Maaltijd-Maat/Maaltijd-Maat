e4df5ac37159ce452baa7066cb45972177151431
René Kok <rkok98@gmail.com>
2020-10-17 17:41:18 +0200

    Added authenticate interceptor

---
+++ src/app/services/authenticate/authenticate.interceptor.ts
@@ -0,0 +1,26 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AuthenticateService } from '@services/authenticate/authenticate.service';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthenticateInterceptor implements HttpInterceptor {
+
+  constructor(public authenticateService: AuthenticateService) {}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const idToken = this.authenticateService.getToken();
+
+    console.log(idToken)
+
+    if (idToken) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${idToken}`
+        }
+      });
+    }
+
+    return next.handle(request);
+  }
+}
