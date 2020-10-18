4496a4ffead530d5ef941dc622abd53ab13dc852
René Kok <rkok98@gmail.com>
2020-10-18 22:55:41 +0200

    Retrieve group

--- src/app/modules/group/group-routing.module.ts
+++ src/app/modules/group/group-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GroupDetails } from './pages/group-details/group-details.component';
+import { GroupResolver } from './pages/group-details/group-resolver.service';
 import { GroupsComponent } from './pages/groups/groups.component';
 import { GroupsResolver } from './pages/groups/groups.resolver';
 import { NewGroupComponent } from './pages/new-group/new-group.component';
 
 const routes: Routes = [
   { path: '', component: GroupsComponent, resolve: { groups: GroupsResolver } },
+  { path: ':id', component: GroupDetails, resolve: { group: GroupResolver } },
   { path: 'new', component: NewGroupComponent }
 ];
 
