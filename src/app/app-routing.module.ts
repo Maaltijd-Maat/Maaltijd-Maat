25812b27891d6e9bb40d3375205c5b7f0b3c0898
René Kok <rkok98@gmail.com>
2020-10-19 21:47:33 +0200

    Rebase

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,8 @@ const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/welcome' },
   { path: 'dishes', loadChildren: () => import('./modules/dish/dishes.module').then(m => m.DishesModule)},
   { path: 'forgot', loadChildren: () => import('./modules/user/forgot/forgot.module').then(m => m.ForgotModule)},
-  { path: 'register', loadChildren: () => import('./modules/register/register.module').then(m => m.RegisterModule) },
-  { path: 'login', loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule) },
+  { path: 'register', loadChildren: () => import('./modules/user/register/register.module').then(m => m.RegisterModule) },
+  { path: 'login', loadChildren: () => import('./modules/user/login/login.module').then(m => m.LoginModule) },
   { path: 'welcome', loadChildren: () => import('./modules/welcome/welcome.module').then(m => m.WelcomeModule) },
   { path: 'groups', loadChildren: () => import('./modules/group/group.module').then(m => m.GroupModule)}
 ];
