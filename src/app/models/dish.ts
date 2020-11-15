c017fce64b6ee3888747e357c63abaa26d610c6d
René Kok <rkok98@gmail.com>
2020-11-15 15:52:06 +0100

    Removed dish implementation

--- src/app/models/dish.ts
+++ src/app/models/dish.ts
@@ -3,31 +3,11 @@ import { IUser } from '@models:/user';
 
 export interface IDish {
   readonly id?: string;
-  readonly author: IUser;
+  readonly author: IUser | null;
 
   name: string;
   amountOfPeople: number;
   ingredients: IIngredient[];
   instructions: string[];
-}
-
-export class Dish implements IDish {
-  readonly id?: string;
-  readonly author?: IUser;
-
-  name: string;
-  amountOfPeople: number;
-  ingredients: IIngredient[];
-  instructions: string[];
-
-  constructor(name: string,
-              instructions: string[], ingredients: IIngredient[], amountOfPeople: number, author?: IUser,) {
-    this.name = name;
-    this.author = author;
-
-    this.instructions = instructions;
-    this.ingredients = ingredients;
-    this.amountOfPeople = amountOfPeople;
-  }
 }
 
