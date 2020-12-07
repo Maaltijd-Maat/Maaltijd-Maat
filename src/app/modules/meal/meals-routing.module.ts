9822119ddd1b63000010743f5b9acbd14cc238f6
René Kok <rkok98@gmail.com>
2020-12-07 12:34:49 +0100

    Created meals overview screen

--- src/app/modules/meal/meals-routing.module.ts
+++ src/app/modules/meal/meals-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MealsComponent } from './pages/groups/meals.component';
+import { MealsResolver } from './pages/groups/meals.resolver';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MealsComponent, resolve: { groups: MealsResolver } }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/meal/meals.module.ts
+++ src/app/modules/meal/meals.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MealService } from '@services/meal/meal.service';
 import { NgZorroAntdModule } from 'ng-zorro-antd';
 import { IconsProviderModule } from '../../icons-provider.module';
 import { MealsRoutingModule } from './meals-routing.module';
+import { MealsComponent } from './pages/groups/meals.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MealsComponent
+  ],
   exports: [],
   imports: [
     CommonModule,
@@ -15,6 +19,8 @@ import { MealsRoutingModule } from './meals-routing.module';
     ReactiveFormsModule,
     MealsRoutingModule
   ],
-  providers: []
+  providers: [
+    MealService
+  ]
 })
 export class MealsModule {}
