4f2f6ccf80d1a13675e027c2b0bcadf36a84434e
René Kok <rkok98@gmail.com>
2020-12-24 15:21:24 +0100

    Week overview QOL changes

--- src/app/modules/meal/meals.module.ts
+++ src/app/modules/meal/meals.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MealService } from '@services/meal/meal.service';
 import { NgZorroAntdModule } from 'ng-zorro-antd';
 import { IconsProviderModule } from '../../icons-provider.module';
@@ -24,7 +24,8 @@ import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
     IconsProviderModule,
     ReactiveFormsModule,
     MealsRoutingModule,
-    CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory })
+    CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory }),
+    FormsModule
   ],
   providers: [
     MealService,
--- src/app/modules/meal/pages/meals/meals.component.ts
+++ src/app/modules/meal/pages/meals/meals.component.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  Inject,
+  LOCALE_ID,
+  ViewChild
+} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { IMeal } from '@models:/meal.model';
 import { MealService } from '@services/meal/meal.service';
 import { CalendarView } from 'angular-calendar';
 import { MealSharedService } from '../../meal.shared.service';
+import { differenceInMinutes, startOfDay, startOfHour } from 'date-fns';
 
 @Component({
   selector: 'app-meals',
   templateUrl: './meals.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrls: ['./meals.component.scss']
 })
-export class MealsComponent {
+export class MealsComponent implements AfterViewInit {
+  @ViewChild('scrollContainer') scrollContainer!: ElementRef<HTMLElement>;
+
   public isNewMealModalVisible: boolean = false;
   public meals: IMeal[] = [];
 
@@ -18,8 +31,11 @@ export class MealsComponent {
   CalendarView = CalendarView;
   view: CalendarView = CalendarView.Week;
 
-  constructor(private route: ActivatedRoute,
+  constructor(@Inject(LOCALE_ID)
+              public locale: string,
+              private route: ActivatedRoute,
               private router: Router,
+              private cdr: ChangeDetectorRef,
               private mealService: MealService,
               private mealSharedService: MealSharedService) {
     // Retrieve list of meals from the meals resolver
@@ -40,13 +56,31 @@ export class MealsComponent {
     this.refreshMeals();
   }
 
+  ngAfterViewInit() {
+    this.scrollToCurrentView();
+  }
+
+  viewChanged() {
+    this.cdr.detectChanges();
+    this.scrollToCurrentView();
+  }
+
+  private scrollToCurrentView() {
+    if (this.view === CalendarView.Week || CalendarView.Day) {
+      // each hour is 60px high, so to get the pixels to scroll it's just the amount of minutes since midnight
+      const minutesSinceStartOfDay = differenceInMinutes(
+        startOfHour(new Date()),
+        startOfDay(new Date())
+      );
+      const headerHeight = this.view === CalendarView.Week ? 60 : 0;
+      this.scrollContainer.nativeElement.scrollTop =
+        minutesSinceStartOfDay + headerHeight;
+    }
+  }
+
   private refreshMeals(): void {
     this.mealService.getMeals().subscribe(meals => {
       this.meals = meals;
     });
-  }
-
-  public isDateInMeals(date: Date): IMeal[] {
-    return this.meals.filter(meal => meal.start == date);
   }
 }
