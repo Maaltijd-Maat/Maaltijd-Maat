d14cdd60bcfb6ed76121816481dbd6ab1d4a6312
René Kok <rkok98@gmail.com>
2021-01-11 23:47:09 +0100

    Removed welcome page and set meals page to homescreen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthenticateService as AuthGuard } from '@services/authenticate/authenticate.service';
 
 const ROUTES: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/groups' },
+  { path: '', pathMatch: 'full', redirectTo: '/meals' },
   { path: 'dishes', loadChildren: () => import('./modules/dish/dishes.module').then(m => m.DishesModule), canActivate: [AuthGuard]},
   { path: 'meals', loadChildren: () => import('./modules/meal/meals.module').then(m => m.MealsModule), canActivate: [AuthGuard] },
   { path: 'forgot', loadChildren: () => import('./modules/user/forgot/forgot.module').then(m => m.ForgotModule)},
   { path: 'register', loadChildren: () => import('./modules/user/register/register.module').then(m => m.RegisterModule) },
   { path: 'login', loadChildren: () => import('./modules/user/login/login.module').then(m => m.LoginModule) },
-  { path: 'welcome', loadChildren: () => import('./modules/welcome/welcome.module').then(m => m.WelcomeModule) },
   { path: 'groups', loadChildren: () => import('./modules/group/group.module').then(m => m.GroupModule), canActivate: [AuthGuard] },
   { path: 'profile', loadChildren: () => import('./modules/user/profile/profile.module').then(m => m.ProfileModule), canActivate: [AuthGuard] },
 ];
