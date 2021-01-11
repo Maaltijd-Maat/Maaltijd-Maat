193701204680a4c87ecde91bbe47a5d52d67a7a5
René Kok <rkok98@gmail.com>
2021-01-11 23:54:19 +0100

    Lazy loading dishes module

--- src/app/modules/dish/dishes.module.ts
+++ src/app/modules/dish/dishes.module.ts
@@ -1,7 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { NgZorroAntdModule } from 'ng-zorro-antd';
+import {
+  NzBreadCrumbModule,
+  NzButtonModule,
+  NzCardModule,
+  NzDividerModule, NzFormModule, NzInputModule, NzInputNumberModule,
+  NzPageHeaderModule, NzPopconfirmModule,
+  NzTableModule
+} from 'ng-zorro-antd';
+
 import { IconsProviderModule } from '../../icons-provider.module';
 
 import { DishesRoutingModule } from './dishes-routing.module';
@@ -9,17 +17,27 @@ import { DishService } from '@services/dish/dish.service';
 import { DishesComponent } from './pages/dishes/dishes.component';
 import { EditDishComponent } from './pages/edit-dish/edit-dish.component';
 import { NewDishComponent } from './pages/new-dish/new-dish.component';
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 @NgModule({
   declarations: [DishesComponent, NewDishComponent, EditDishComponent],
   exports: [DishesComponent, NewDishComponent, EditDishComponent],
   imports: [
     CommonModule,
-    NgZorroAntdModule,
     IconsProviderModule,
     ReactiveFormsModule,
-    DishesRoutingModule
+    DishesRoutingModule,
+    NzPageHeaderModule,
+    NzBreadCrumbModule,
+    NzCardModule,
+    NzDividerModule,
+    NzButtonModule,
+    NzTableModule,
+    NzFormModule,
+    NzInputModule,
+    NzInputNumberModule,
+    NzPopconfirmModule
   ],
-  providers: [DishService]
+  providers: [DishService, NzMessageService]
 })
 export class DishesModule {}
